0c7fd6999d76d67d3fe583c9b0fb79b0308189a4
Martin Vrachev <martin.vrachev@gmail.com>
2018-11-12 17:45:56 +0200

    Update solution.go

--- 02/solution.go
+++ 02/solution.go
@@ -11,12 +11,6 @@ type Editor interface {
 	// Delete length items from offset.
 	Delete(offset, length uint) Editor
 
-	//Undo reverts latest change.
-	//Undo() Editor
-
-	// Redo re-applies latest undone change.
-	//Redo() Editor
-
 	// String returns complete representation of what a file looks
 	// like after all manipulations.
 	String() string
